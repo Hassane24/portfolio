a1e4c320b77af442954f7771949f589a1cd5e340
Hassane <hsisane5@gmail.com>
2023-07-13 19:13:00 +0100

    animation and styling to landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
+import styles from "../styles/landingPage.module.css";
 export const LandingPage = () => {
+  const [animate, setAnimate] = useState(false);
+  useEffect(() => setAnimate(true), []);
   return (
-    <div>
-      <span>Hassane Ben</span>
-      <span>Front-End Web Developer</span>
+    <div className={styles.container}>
+      <span className={`${styles.name} ${animate ? styles.animate : ""}`}>
+        Hassane Ben
+      </span>
+      <span className={`${styles.occupation} ${animate ? styles.animate : ""}`}>
+        Front-End Web Developer
+      </span>
     </div>
   );
 };
