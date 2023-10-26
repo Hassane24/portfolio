a5a05e1427be8343adaffef1d5386849a7d2b253
Hassane <hsisane5@gmail.com>
2023-10-26 20:04:24 +0100

    styling of about me page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,37 +8,41 @@ export const About = ({ name }: { name: string }) => {
         problem-solving skills to design and implement websites for clients.
       </div>
       <div className={styles.skills}>
-        <div className={styles.skill}>
-          <i className="devicon-html5-plain colored"></i>
-          <span>HTML</span>
+        <div>
+          <div className={styles.skill}>
+            <i className="devicon-html5-plain colored icon"></i>
+            <span>HTML</span>
+          </div>
+          <div className={styles.skill}>
+            <i className="devicon-css3-plain colored icon"></i>
+            <span>CSS</span>
+          </div>
+          <div className={styles.skill}>
+            <i className="devicon-javascript-plain colored icon"></i>
+            <span>JAVASCRIPT</span>
+          </div>
+          <div className={styles.skill}>
+            <i className="devicon-git-plain colored icon"></i>
+            <span>GIT</span>
+          </div>
         </div>
-        <div className={styles.skill}>
-          <i className="devicon-css3-plain colored"></i>
-          <span>CSS</span>
-        </div>
-        <div className={styles.skill}>
-          <i className="devicon-javascript-plain colored"></i>
-          <span>JAVASCRIPT</span>
-        </div>
-        <div className={styles.skill}>
-          <i className="devicon-git-plain colored"></i>
-          <span>GIT</span>
-        </div>
-        <div className={styles.skill}>
-          <i className="devicon-jest-plain colored"></i>
-          <span>JEST</span>
-        </div>
-        <div className={styles.skill}>
-          <i className="devicon-react-original colored"></i>
-          <span>REACT</span>
-        </div>
-        <div className={styles.skill}>
-          <i className="devicon-firebase-plain colored"></i>
-          <span>FIREBASE</span>
-        </div>
-        <div className={styles.skill}>
-          <i className="devicon-typescript-plain colored"></i>
-          <span>TYPESCRIPT</span>
+        <div>
+          <div className={styles.skill}>
+            <i className="devicon-jest-plain colored icon"></i>
+            <span>JEST</span>
+          </div>
+          <div className={styles.skill}>
+            <i className="devicon-react-original colored icon"></i>
+            <span>REACT</span>
+          </div>
+          <div className={styles.skill}>
+            <i className="devicon-firebase-plain colored icon"></i>
+            <span>FIREBASE</span>
+          </div>
+          <div className={styles.skill}>
+            <i className="devicon-typescript-plain colored icon"></i>
+            <span>TYPESCRIPT</span>
+          </div>
         </div>
       </div>
     </section>
